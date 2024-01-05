2e7b89be4455dc0ac90c4cd78f1464e5fa9e30b3
SeungTaek5 <dizzyrains@gmail.com>
2024-01-05 18:14:57 +0900

    과제1 진행중

---
+++ src/과제/과제1/ex1/Level1_1.java
@@ -0,0 +1,34 @@
+package 과제.과제1.ex1;
+
+public class Level1_1 {
+	
+	public static void main(String[] args) {
+		System.out.println(" >> Level1_1 answer >> \n");
+		/* 문제풀이 위치 */
+
+		boolean a = true;
+		byte b = 100;
+		char c = 'A';
+		short d = 30000;
+		int e = 2000000000;
+		long f = 40000000000l;
+		float g = 3.1231232f;
+		double h = 3.123123123;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		/* ----------- */
+	}
+}
+/* 
+	[문제] 그림과 같이 기본자료형을 이용하여 변수 8개 선언하고 임의값을 대입하여 출력하시오.
+	[조건] 
+		1. 모든 기본자료형 8개 사용
+		2. println() 메소드 사용
+*/
